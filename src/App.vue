cb74e106b92ea45e636aac73266e4526f892476e
Diego Pereira <diegoteixeirapereira@hotmail.com>
2021-07-07 22:24:48 -0300

    criação do componente vue ProdutoBox.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
 <template>
-  <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+     <navbar />
+     <div style="min-height: 60vh">
+       <router-view></router-view>
+     </div>
+     <Footer />
 </template>
 
+
+
+<script>
+import Navbar from "./components/Navbar.vue"
+import Footer from "./components/Footer.vue"
+export default {
+  data() {
+    return {
+      baseURL : "https://limitless-lake-55070.herokuapp.com/",
+      //baseURL : "http://localhost:8080/api/",
+      products : null,
+      categories : null,
+      key : 0
+    }
+  },
+  
+  components : { Footer , Navbar},
+  
+   
+}
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <router-link class="navbar-brand" :to="{name : 'Home'}">
+      <img id="logo" src="" />
+    </router-link>
+  </nav>
+
+</template>
